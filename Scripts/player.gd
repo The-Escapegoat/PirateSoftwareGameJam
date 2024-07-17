76886034cae6d15350af15650b9356f1b64c8f3f
sdrshadow <torinrauch2008@gmail.com>
2024-07-17 16:43:56 -0700

    Movement

---
+++ Scripts/player.gd
@@ -0,0 +1,56 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const ACCELERATION = 1400.0
+const FRICTION = 1400.0
+const JUMP_VELOCITY = -300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyotye_jump_timer = $CoyoteJumpTimer
+
+
+func _physics_process(delta):
+	apply_gravity(delta)
+	handle_jump()
+	var input_axis = Input.get_axis("Left", "Right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	update_animations(input_axis)
+	var was_on_floor = is_on_floor()
+	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyotye_jump_timer.start()
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+func handle_jump():
+	if is_on_floor() or coyotye_jump_timer.time_left > 0.0:		
+		if Input.is_action_just_pressed("Jump"):
+			velocity.y = JUMP_VELOCITY
+	if not is_on_floor():
+		if Input.is_action_just_released("Jump") and velocity.y < JUMP_VELOCITY/2:
+			velocity.y = JUMP_VELOCITY/2
+			
+func apply_friction(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+		
+func update_animations(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
